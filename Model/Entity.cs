b5a4a092a4fd4736face00293f5bc80abda9cd26
csant714 <csant714@students.bju.edu>
2017-11-06 19:54:55 -0500

    Entity complete for now

--- Model/Entity.cs
+++ Model/Entity.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace TowerOfTerror.Model
 {
+    // Enumerated type that determines whether an entity is alive, dead, or falling.
+    public enum Life { Alive, Dead, Falling };
+
     // Contains general information for all living things
-    class Entity
+    abstract class Entity
     {
-        public Entity()
-        {
-            // Do stuff.
-        }
+        public int Id { get; }
+        public string Image { get; set; }
+        public Point Position { get; set; }
+        public int Power { get; set; }
+        public int Defense { get; set; }
+        public int Health { get; set; }
+        public Life Status { get; set; }
+
+        // Whoever you are, do an attack
+        public abstract void Attack();
+
+        // Is the entity dead? Handle each differently
+        public abstract bool IsDead();
     }
 }
