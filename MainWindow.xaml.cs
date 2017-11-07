7f76a86e315d4cb0341669ebbaf2bea1aa0e88fc
jmauk652 <jmauk652@students.bju.edu>
2017-11-07 14:33:59 -0500

    Changed comment

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace TowerOfTerror
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml Mehoymeny
     /// </summary>
     public partial class MainWindow : Window
     {
