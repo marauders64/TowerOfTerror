b1f266f891991ce9008313e2c9932bc3fec6a8e1
csant714 <csant714@students.bju.edu>
2017-11-07 19:58:42 -0500

    Added basic level

---
+++ Model/Level.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerOfTerror.Model
+{
+    // Contains data for the levels
+    class Level
+    {
+        public static int num = 0;
+        public int Num { get; set; }
+    }
+}
