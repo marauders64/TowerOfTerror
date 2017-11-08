f538f9a8ca8fd3b29b57b55e460d038b7082cd33
heast <heast@bju.edu>
2017-11-07 20:21:22 -0500

    attempting to commit model unit test stub

---
+++ Model/ModelTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TowerOfTerror.Model
+{
+    [TestClass]
+    class ModelTest
+    {
+        public void Attack_EnemyTakesDamage()
+        {
+            Character hero = new Character();
+            Enemy villain = new Enemy();
+
+        }
+    }
+}
