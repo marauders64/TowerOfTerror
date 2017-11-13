1dc6ed0b557b75eb295ddcf5f37a194c709dcf5b
csant714 <csant714@students.bju.edu>
2017-11-13 00:17:36 -0500

    Added logic for About, Help, and HighScores buttons. Note: clicking on the high scores button while the list of high scores is null crashes the app.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TowerOfTerror.Model;
 
 namespace TowerOfTerror
 {
@@ -31,27 +32,57 @@ namespace TowerOfTerror
 
         private void btnStartGame_Click(object sender, RoutedEventArgs e)
         {
-
+            // Start the Game
         }
 
         private void btnLoadGame_Click(object sender, RoutedEventArgs e)
         {
-
+            // Load a file
         }
 
         private void btnAbout_Click(object sender, RoutedEventArgs e)
         {
+            string aboutText = @"Tower Of Terror
+Produced by:
+    Heather East
+    Jonathan Mauk
+    Carlos Santana
 
+Inspired by: The Legend of Zelda: A Link Between Worlds";
+            MessageBoxButton exit = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Information;
+            MessageBox.Show(aboutText, "About", exit, icon);
         }
 
         private void btnHelp_Click(object sender, RoutedEventArgs e)
         {
-
+            string helpText = @"Controls:
+Move: WASD or Arrow Keys
+Attack: Space Bar
+Use Item: Click on it in the sidebar (100 level)";
+            MessageBoxButton exit = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Information;
+            MessageBox.Show(helpText, "How to Play", exit, icon);
         }
 
+        // Show a screen with the high scores
         private void btnHighScores_Click(object sender, RoutedEventArgs e)
         {
-
+            string scores = "High Scores thus far:\n";
+            if (HighScores.Leaderboard.Scores == null)
+            {
+                scores = "No high scores to show.";
+            }
+            else
+            {
+                foreach (HighScore score in HighScores.Leaderboard.Scores)
+                {
+                    scores += score + "\n";
+                }
+            }
+            MessageBoxButton exit = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Information;
+            MessageBox.Show(scores, "How to Play", exit, icon);
         }
     }
 }
